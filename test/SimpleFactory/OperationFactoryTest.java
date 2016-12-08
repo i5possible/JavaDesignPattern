64dc81fcdf312ff64348a74d3d2e160c21a9a31f
Liang Hong <hliang@CNhliang.local>
2016-12-08 16:12:47 +0800

    Add Operation Factory Test. Only for OperationAdd and OperationMinus.

---
+++ test/SimpleFactory/OperationFactoryTest.java
@@ -0,0 +1,21 @@
+package SimpleFactory;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by hliang on 08/12/2016.
+ */
+public class OperationFactoryTest {
+    @Test
+    public void OperationAddTest () {
+        Operation operation = new OperationFactory().createOperation("+").get();
+        Assert.assertEquals(3.3,operation.getResult(1.1,2.2),1e-9);
+    }
+
+    @Test
+    public void OperationMinusTest () {
+        Operation operation = new OperationFactory().createOperation("-").get();
+        Assert.assertEquals(-1.1,operation.getResult(1.1,2.2),1e-9);
+    }
+}
