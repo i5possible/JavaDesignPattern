23f9e61e36b00d3e8a38902281ea577f4956c6b1
Liang Hong <lianghongbuaa@gmail.com>
2016-12-09 09:35:21 +0800

    Add FactoryMethod Test.

---
+++ test/FactoryMethod/IOperationFactoryTest.java
@@ -0,0 +1,21 @@
+package FactoryMethod;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by hliang on 08/12/2016.
+ */
+public class IOperationFactoryTest {
+    @Test
+    public void OperationAddTest () {
+        IOperation ioperation = new OperationAddFactory().getOperation();
+        Assert.assertEquals(3.3,ioperation.getResult(1.1,2.2),1e-9);
+    }
+
+    @Test
+    public void OperationMinusTest () {
+        IOperation ioperation = new OperationMinusFactory().getOperation();
+        Assert.assertEquals(-1.1,ioperation.getResult(1.1,2.2),1e-9);
+    }
+}
