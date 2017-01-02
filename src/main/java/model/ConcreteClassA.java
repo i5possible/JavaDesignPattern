a72b660e844d0797d5b7f216f081b8252a7e89b3
Liang Hong <lianghongbuaa@gmail.com>
2017-01-02 09:39:51 +0800

    Add model method.

---
+++ src/main/java/model/ConcreteClassA.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ConcreteClassA extends AbstractClass {
+    @Override
+    public void primitiveOperation1() {
+        System.out.println("This is ConcreteClassA operation1.");
+    }
+
+    @Override
+    public void primitiveOperation2() {
+        System.out.println("This is ConcreteClassA operation2.");
+    }
+}
---
+++ src/main/java/model/ConcreteClassB.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ConcreteClassB extends AbstractClass {
+    @Override
+    public void primitiveOperation1() {
+        System.out.println("This is ConcreteClassB operation1.");
+    }
+
+    @Override
+    public void primitiveOperation2() {
+        System.out.println("This is ConcreteClassB operation2.");
+    }
+}
