b555ca1abb1ed5e7729ad4de7dfd95ab0a8dd669
Liang Hong <lianghongbuaa@gmail.com>
2017-01-02 09:56:24 +0800

    Add Lod principle.

---
+++ src/main/java/designprinciple/Lod.java
@@ -0,0 +1,8 @@
+package designprinciple;
+
+public class Lod {
+    /**
+     * Lod : When two class don't need to communicate with each other.
+     * Then the two class should not interact with each other.
+     */
+}
