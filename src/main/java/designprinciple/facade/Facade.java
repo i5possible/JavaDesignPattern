0dafca320ff3485dc8861562da64deceb0ebedb4
Liang Hong <lianghongbuaa@gmail.com>
2017-01-11 21:39:45 +0800

    Add facade method to the project. The facade method is a very useful design patter. We use it almost every where. It's cool to find out it when you are coding or reading other's code.

---
+++ src/main/java/designprinciple/facade/Facade.java
@@ -0,0 +1,72 @@
+package designprinciple.facade;
+
+/**
+ * Facade provide a consistence interface for a couple of class.
+ * It's easier to call many classes' method in a giving order.
+ *
+ * 1. Facade is a nice implementation of LoD and DependencyInversion. So, when you do the design of a system, you
+ *      can try to isolate different layer and use Facade to simplify the design.
+ * 2. When ths class and the method become more and more that you barely can tell which method or class is the best
+ *      choice to use, you need to use facade to package some of them to make the code clearer.
+ * 3. When you have a existing system and you have to interact with it, especially ths system have tough design or
+ *      bad code quality, and you don't want to refactor the code, you can add some facades to them. So you don't need
+ *      to deal with the system directly, you interact with the facade only.
+ */
+public class Facade {
+    SubSystemOne subSystemOne;
+    SubSystemTwo subSystemTwo;
+    SubSystemThree subSystemThree;
+    SubSystemFour subSystemFour;
+
+    public Facade(SubSystemOne subSystemOne, SubSystemTwo subSystemTwo, SubSystemThree subSystemThree, SubSystemFour subSystemFour) {
+        this.subSystemOne = subSystemOne;
+        this.subSystemTwo = subSystemTwo;
+        this.subSystemThree = subSystemThree;
+        this.subSystemFour = subSystemFour;
+    }
+
+    public void methodA () {
+        subSystemOne.methodOne();
+        subSystemTwo.methodtTwo();
+        subSystemFour.methodFour();
+    }
+    
+    public  void methodB () {
+        subSystemOne.methodOne();
+        subSystemThree.methodThree();
+    }
+
+    public static void main(String[] args) {
+        SubSystemOne subSystemOne = new SubSystemOne();
+        SubSystemTwo subSystemTwo = new SubSystemTwo();
+        SubSystemThree subSystemThree = new SubSystemThree();
+        SubSystemFour subSystemFour = new SubSystemFour();
+        Facade facade = new Facade(subSystemOne, subSystemTwo, subSystemThree, subSystemFour);
+        facade.methodA();
+        facade.methodB();
+    }
+}
+
+class SubSystemOne {
+    public void methodOne() {
+        System.out.println("methodOne");
+    }
+}
+
+class SubSystemTwo {
+    public void methodtTwo() {
+        System.out.println("methodTwo");
+    }
+}
+
+class SubSystemThree {
+    public void methodThree() {
+        System.out.println("methodThree");
+    }
+}
+
+class SubSystemFour {
+    public void methodFour() {
+        System.out.println("Four");
+    }
+}
