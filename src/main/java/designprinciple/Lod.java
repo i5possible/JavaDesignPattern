f999f94b67c323d3c6607c1ba7db0d422e785027
Liang Hong <lianghongbuaa@gmail.com>
2017-01-11 21:39:36 +0800

    Add comment to Lod principle.

--- src/main/java/designprinciple/Lod.java
+++ src/main/java/designprinciple/Lod.java
@@ -2,6 +2,7 @@ package designprinciple;
 
 public class Lod {
     /**
+     * LKP : Least Knowledge principle
      * Lod : When two class don't need to communicate with each other.
      * Then the two class should not interact with each other.
      */
